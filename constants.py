41792164262be44653c11581f4b95e5e26c70603
alfredoTous <adtous@uninorte.edu.co>
2025-05-30 18:53:40 -0500

    update: added levels

--- constants.py
+++ constants.py
@@ -2,7 +2,15 @@ WIDTH, HEIGHT = 1280, 720
 FPS = 60
 
 levels = [
-    {"duration": 30, "velocity": (3,6), "extra_lives": 5,"scale":(80,80), "trash_spawn_denominator": 50},
-    {"duration": 35, "velocity": (3,6), "extra_lives": 4,"scale":(70,70), "trash_spawn_denominator": 40},
-    {"duration": 40, "velocity": (4,7), "extra_lives": 3,"scale":(60,60), "trash_spawn_denominator": 30},
+    {"duration": 30, "velocity": (3, 6), "extra_lives": 15, "scale": (80, 80), "trash_spawn_denominator": 50},
+    {"duration": 32, "velocity": (3, 6), "extra_lives": 14, "scale": (78, 78), "trash_spawn_denominator": 47},
+    {"duration": 35, "velocity": (4, 6), "extra_lives": 13, "scale": (75, 75), "trash_spawn_denominator": 44},
+    {"duration": 38, "velocity": (4, 7), "extra_lives": 11, "scale": (70, 70), "trash_spawn_denominator": 41},
+    {"duration": 41, "velocity": (5, 7), "extra_lives":  9, "scale": (66, 66), "trash_spawn_denominator": 38},
+    {"duration": 44, "velocity": (5, 8), "extra_lives":  8, "scale": (62, 62), "trash_spawn_denominator": 35},
+    {"duration": 47, "velocity": (5, 8), "extra_lives":  7, "scale": (58, 58), "trash_spawn_denominator": 32},
+    {"duration": 50, "velocity": (6, 8), "extra_lives":  6, "scale": (54, 54), "trash_spawn_denominator": 29},
+    {"duration": 53, "velocity": (6, 9), "extra_lives":  5, "scale": (50, 50), "trash_spawn_denominator": 23},
+    {"duration": 55, "velocity": (6, 9), "extra_lives":  5, "scale": (48, 48), "trash_spawn_denominator": 18},
 ]
+
